83eccb9174fbe37576882ac5e496075826f82bb6
Tawnee Harris <tawneeh@icloud.com>
2021-01-19 10:26:16 -0800

    add lesson 6 and 7

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using CretaceousPark.Models;
+using Microsoft.EntityFrameworkCore; // needed to use EntityState
 
 namespace CretaceousPark.Controllers
 {
@@ -31,11 +32,29 @@ namespace CretaceousPark.Controllers
       _db.SaveChanges();
     }
 
-    // GET api/animals/5 --- getting a 404
+    // GET api/animals/5
     [HttpGet("{id}")]
     public ActionResult<Animal> Get(int id)
     {
       return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+    }
+
+    // PUT api/animals/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Animal animal) // PUT is like POST in that it makes a change to the server. But PUT changes exsting info while POST creates new info
+    {
+        animal.AnimalId = id;
+        _db.Entry(animal).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+
+    // DELETE api/animals/5
+    [HttpDelete("{id}")]
+    public void Delete(int id) //the code is exactly the same as what we'd see in a web application (except for HttpDelete). Entity doesn't care whether it gets information from an API or a web application when manipulating the database
+    {
+      var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+      _db.Animals.Remove(animalToDelete);
+      _db.SaveChanges();
     }
   }
 }
