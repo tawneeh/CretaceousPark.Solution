2dac8f11368b32e1091d74d8e20fbebf44c1f37b
Tawnee Harris <tawneeh@icloud.com>
2021-01-19 21:58:55 -0800

    add lesson 18

--- CretaceousClient.Solution/CretaceousClient/Models/Animal.cs
+++ CretaceousClient.Solution/CretaceousClient/Models/Animal.cs
@@ -34,5 +34,22 @@ namespace CretaceousClient.Models
 
       return animal;
     }
+
+    public static void Post(Animal animal)
+    {
+      string jsonAnimal = JsonConvert.SerializeObject(animal);
+      var apiCallTask = ApiHelper.Post(jsonAnimal);
+    }
+
+    public static void Put(Animal animal)
+    {
+      string jsonAnimal = JsonConvert.SerializeObject(animal);
+      var apiCallTask = ApiHelper.Put(animal.AnimalId, jsonAnimal);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
+    }
   }
 }
